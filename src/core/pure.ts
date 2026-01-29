d3b25aa0a6da69c472b906c3224724b8a1384f0a
Leawind <leawind@yeah.net>
2026-01-29 21:16:34 +0800

    refactor(types): move DeepPartial and DeepRequire

--- src/core/pure.ts
+++ src/core/pure.ts
@@ -6,3 +6,11 @@ export type Access<T, K extends PropertyKey, E = never> = K extends keyof T ? T[
 
 export type AppendDoc<A, Doc> = Pick<A & Doc, keyof A>
 export type PrependDoc<A, Doc> = Pick<Doc & A, keyof A>
+
+export type DeepPartial<T> = {
+  [P in keyof T]?: T[P] extends object ? DeepPartial<T[P]> : T[P]
+}
+
+export type DeepRequire<T> = {
+  [P in keyof T]-?: T[P] extends object ? DeepRequire<T[P]> : T[P]
+}
--- src/index.ts
+++
@@ -1,7 +0,0 @@
-export type DeepPartial<T> = {
-  [P in keyof T]?: T[P] extends object ? DeepPartial<T[P]> : T[P]
-}
-
-export type DeepRequire<T> = {
-  [P in keyof T]-?: T[P] extends object ? DeepRequire<T[P]> : T[P]
-}
