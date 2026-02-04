45233471a04a85cd39be3f5dc02287288a800436
Leawind <leawind@yeah.net>
2026-02-04 14:16:16 +0800

    feat(scripts): upgrade inventory dependency and enhance index generation

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,11 +1,7 @@
 import { program } from 'npm:commander@^14.0'
-import { FilePath, Path } from 'jsr:@leawind/inventory@^0.17.4/fs'
-import log, { LevelLike } from 'jsr:@leawind/inventory@^0.17.4/log'
-import { r } from 'jsr:@leawind/inventory@^0.17.4/tstr'
-
-const RGX_INDEX_EXPORTS = /((^\/\/ Index start >{16}\n)((.*)(^\/\/ <{16}   Index end$)?)?)/ms
-const START_LINE = `// Index start >>>>>>>>>>>>>>>>`
-const END_LINE = `// <<<<<<<<<<<<<<<<   Index end`
+import { DirPath, FilePath, Path, PathLike } from 'jsr:@leawind/inventory@^0.17.5/fs'
+import log, { LevelLike } from 'jsr:@leawind/inventory@^0.17.5/log'
+import { I, r } from 'jsr:@leawind/inventory@^0.17.5/tstr'
 
 type CliOptions = {
   check: boolean
@@ -13,69 +9,83 @@ type CliOptions = {
   verbose: boolean
 }
 
-program
-  .name('index')
-  .description(
-    `Generate or check index files.\n\nAdd this line to the index.ts file where the index is generated: \n// Index start >>>>>>>>>>>>>>>>`,
-  )
-  .argument('<dir>', 'Directory to generate index files for')
-  .option('-c, --check', 'Check if index files are good', false)
-  .option('-q, --quiet', 'Quiet', false)
-  .option('-v, --verbose', 'Verbose', false)
-  .action(async (dir: string, options: CliOptions) => {
-    if (options.quiet && options.verbose) {
-      log.error('ERROR: --quiet and --verbose cannot be used at the same time')
-      Deno.exit(1)
-    }
+if (import.meta.main) {
+  program
+    .name('index')
+    .description(`Generate or check index.ts recursively in a directory`)
+    .argument('<dir>', 'Directory to generate index files for')
+    .option('-c, --check', 'Check if index files are good', false)
+    .option('-q, --quiet', 'Quiet', false)
+    .option('-v, --verbose', 'Verbose', false)
+    .action(async (dir: string, options: CliOptions) => {
+      if (options.quiet && options.verbose) {
+        log.error('ERROR: --quiet and --verbose cannot be used at the same time')
+        Deno.exit(1)
+      }
+      log.api.setLevel(options.verbose ? 'trace' : options.quiet ? 'none' : 'info')
 
-    log.api.setLevel(options.verbose ? 'trace' : options.quiet ? 'none' : 'info')
+      const dirties = await genIndex(await Path.dir(dir), {
+        check: options.check,
+      })
 
-    const dirties = await genIndex(await Path.dir(dir), {
-      check: options.check,
+      if (dirties.length > 0) {
+        log.error('FAIL: There are dirty index files:')
+        dirties.forEach((file) => log.error('  ' + file))
+        Deno.exit(1)
+      } else {
+        log.info('PASS: All index files are good')
+      }
     })
-    if (dirties.length > 0) {
-      log.error('FAIL: There are dirty index files:')
-      dirties.forEach((file) => log.error('  ' + file))
-      Deno.exit(1)
-    } else {
-      log.info('PASS: All index files are good')
-    }
-  })
-  .parse([Deno.execPath(), ...Deno.args])
+    .parse([Deno.execPath(), ...Deno.args])
+}
 
 export type Options = {
   depth: number
   check: boolean
+  startLine: string
+  endLine: string
+  startLinePattern?: RegExp
+  endLinePattern?: RegExp
+  exportStatement(path: string): string
+  fileFilter: (path: FilePath) => boolean
+  dirFilter: (path: DirPath) => boolean
 }
-export async function genIndex(path: Path, options: Partial<Options>): Promise<FilePath[]> {
-  const opts = Object.assign({
+export async function genIndex(path: PathLike, options: Partial<Options>): Promise<FilePath[]> {
+  const opts: Options = Object.assign({
     depth: 0,
     check: true,
+    startLine: `// Index start >>>>>>>>>>>>>>>>`,
+    endLine: `// <<<<<<<<<<<<<<<<   Index end`,
+    startLinePattern: /\/\/ +Index start +>>>>>>>>>>>>>>>>/,
+    endLinePattern: /\/\/ +<<<<<<<<<<<<<<<< +Index end/,
+    exportStatement: (path: string) => `export * from '${path}'`,
+    fileFilter: (path: FilePath) => /.*\.ts$/.test(path.name) && !/(^index\.ts$)|(.*\.test\.ts$)/.test(path.name),
+    dirFilter: (path: DirPath) => true,
   }, options)
 
-  const IDE = '  '.repeat(opts.depth)
-  const dir = await path.asDir()
+  const indent = '  '.repeat(opts.depth)
+  const dir = await Path.dir(path)
 
   const index_ts = await dir.join('index.ts').asFile(false)
-  log.trace(IDE + index_ts)
+  log.trace(indent + index_ts)
 
   const statements: string[] = []
   const dirties: FilePath[] = []
 
   const children = (await dir.list())
-    .filter((child) => !/(^index\.ts$)|(.*\.test\.ts$)/.test(child.name))
+    .filter((path: Path) => path.matchSync({ file: opts.fileFilter, dir: opts.dirFilter }))
     .sort((a, b) => a.path.localeCompare(b.path))
   for (const child of children) {
     await child.match({
       file(path) {
-        const statement = `export * from './${path.name}'`
-        log.trace(IDE + statement)
+        const statement = opts.exportStatement(`./${path.name}`)
+        log.trace(indent + statement)
         statements.push(statement)
       },
       async dir(path) {
         if ((await path.list()).length > 0) {
-          const statement = `export * from './${path.name}/index.ts'`
-          log.trace(IDE + statement)
+          const statement = opts.exportStatement(`./${path.name}/index.ts`)
+          log.trace(indent + statement)
           statements.push(statement)
           dirties.push(...await genIndex(path, Object.assign(opts, { depth: opts.depth + 1 })))
         }
@@ -85,28 +95,52 @@ export async function genIndex(path: Path, options: Partial<Options>): Promise<F
 
   const content = await index_ts.isFile() ? await index_ts.readText() : ''
 
-  if (!RGX_INDEX_EXPORTS.test(content)) {
-    log.trace(IDE + `${index_ts.path} ...Skip: Mark not found`)
+  // Find start and end lines using string or regex matching
+  const lines = content.split('\n')
+
+  const matcher: (pattern: string | RegExp) => (line: string) => boolean = (pattern: string | RegExp) => {
+    if (pattern instanceof RegExp) {
+      return (line: string) => pattern.test(line)
+    }
+    return (line: string) => line === pattern
+  }
+
+  const startIndex = lines.findIndex(matcher(opts.startLinePattern ?? opts.startLine))
+  const endIndex = startIndex >= 0
+    ? lines.slice(startIndex).findIndex(matcher(opts.endLinePattern ?? opts.endLine))
+    : -1
+
+  if (startIndex === -1) {
+    log.trace(indent + `${index_ts.path} ...Skip: Mark not found`)
     return dirties
   }
 
-  const replaced = content
-    .replace(RGX_INDEX_EXPORTS, [START_LINE, ...statements, END_LINE].join('\n'))
-    .replace(/(?!\n|^)$/m, '\n')
-  const isDirty = content.trim() !== replaced.trim()
+  const newIndexContent = [opts.startLine, ...statements, opts.endLine].join('\n')
 
-  if (isDirty) {
+  if (endIndex === -1) {
+    lines.splice(startIndex, Infinity, newIndexContent)
+  } else {
+    lines.splice(startIndex, endIndex + 1, newIndexContent)
+  }
+
+  let replaced = lines.join('\n')
+  if (!replaced.endsWith('\n')) {
+    replaced += '\n'
+  }
+
+  if (content.trim() !== replaced.trim()) {
+    log.trace(`${index_ts.path} ...Replace with:\n${newIndexContent}`)
     if (opts.check) {
-      log.trace(IDE + c(91)`${index_ts.path} ...Dirty`)
+      log.trace(indent + c(91)`${index_ts.path} ...Dirty`)
       return [...dirties, index_ts]
     } else {
       await index_ts.write(replaced)
-      log.trace(IDE + c(92)`${index_ts.path} ...Updated`)
+      log.trace(indent + c(92)`${index_ts.path} ...Updated`)
       return []
     }
   }
 
-  log.trace(IDE + c(92)`${index_ts.path} ...Good`)
+  log.trace(indent + c(92)`${index_ts.path} ...Good`)
   return dirties
 
   function c(id: number) {
