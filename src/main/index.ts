13c40aebc77f7209356faf10db9eb6bf1374a201
Leawind <leawind@yeah.net>
2026-02-08 20:30:23 +0800

    refactor(main): remove compare

--- src/main/index.ts
+++ src/main/index.ts
@@ -4,37 +4,11 @@
  * @module
  */
 
-import type { CompareTypes } from './compare.ts'
 import type { ExpectType } from './expect.ts'
 import { NOOP } from './noop.ts'
 
-export type { CompareTypes } from './compare.ts'
 export type { ExpectType } from './expect.ts'
 export { NOOP } from './noop.ts'
-
-/**
- * Creates an instance of CompareTypes to perform type-level comparisons between two types.
- * This function enables testing various relationships between types at compile time.
- * NOTE: This function does nothing at runtime and is purely for type-level testing.
- *
- * @template T First type to compare
- * @template U Second type to compare
- *
- * @returns A CompareTypes instance with methods to test relationships between T and U
- *
- * @example
- * ```ts
- * // Compare two identical types
- * compare<number, number>().same // Results in an available property
- * // Compare two different but overlapping types
- * compare<4, number>().overlap.different // Results in available properties
- * ```
- */
-export function compare<T, U>(): CompareTypes<T, U>
-export function compare<T, U>(t: T, u: U): CompareTypes<T, U>
-export function compare<T, U>(): CompareTypes<T, U> {
-  return NOOP
-}
 
 /**
  * Creates an instance of ExpectType to perform type-level assertions on the given type.
